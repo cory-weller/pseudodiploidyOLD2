b980299c7e2d17d59e8282881545cd0e53b89789
Cory <cory.a.weller@gmail.com>
2022-01-06 16:38:50 +0000

    add plotting examples

--- src/run-contrast.R
+++ src/run-contrast.R
@@ -31,11 +31,32 @@ group2 <- "a_pseudo"
 
 expressionContrast <- runContrast(dds, group1, group2)
 
+# add isSignif column, where TRUE if log2FoldChange and padj pass threshold
+expressionContrast[abs(log2FoldChange) > 2 & padj < 0.05, isSignif := TRUE]
+
+# add gene label column only for significant genes
+expressionContrast[isSignif==TRUE, geneLabel := geneID]
+
+# make column for scatter plot alpha, where insignif genes are see-through
+expressionContrast[, plotAlpha := 0.3]
+expressionContrast[isSignif==TRUE, plotAlpha := 1.0]
+
+ggplot(data=expressionContrast, 
+        aes(
+            x=log2FoldChange, 
+            y=-1*log10(padj), 
+            label=geneLabel, 
+            alpha=plotAlpha
+        ) + 
+        geom_point(shape=21, alpha=0.3) +
+        geom_text()
 
 
+# get list of upregulated genes
+expressionContrast[log2FoldChange > 2 & padj < 0.05, geneID]
 
-
-
+# get list of downregulated genes
+expressionContrast[log2FoldChange < -2 & padj < 0.05, geneID]
 
 
 
